c649d6b4fafb5aa295f70f6e5e937301bbc9bef7
tomcli <tomcli@tencent.com>
2023-07-20 10:42:11 +0800

    add load launch.json mapping

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -27,6 +29,9 @@ return {
     ["<leader>xa"] = { "<cmd>:AsyncTask build-and-run-all<cr>", desc = "all" },
     ["<leader>xp"] = { "<cmd>:AsyncTask protoc<cr>", desc = "protoc" },
     ["<leader>xd"] = { "<cmd>:AsyncTask build-for-debug<cr>", desc = "build current svr for debug" },
+
+    -- dap
+    ["<leader>dl"] = { "<cmd>:DapLoadLaunchJSON<cr>", desc = "load launch.json" },
   },
   t = {
     -- setting a mapping to false will disable it
